5096b92826ca32f3db4136a4d2c1d25ae2a64023
Louis Rousseau <louisrousseau@gmail.com>
2026-09-12 19:56:16 -0400

    Re-enable apk add for dependencies

--- install.sh
+++ install.sh
@@ -30,12 +30,11 @@ fi
 # PRE
 ################################################################################
 
-# Packages now included in my build config
-#sudo apk add git unzip libffi-dev make gcc g++ \
-#ncurses-dev avrdude gcc-avr binutils-avr avr-libc \
-#python3 py3-virtualenv \
-#python3-dev freetype-dev fribidi-dev harfbuzz-dev jpeg-dev lcms2-dev openjpeg-dev tcl-dev tiff-dev tk-dev zlib-dev \
-#jq patch libsodium caddy curl polkit grep iproute2
+sudo apk add git unzip libffi-dev make gcc g++ \
+ncurses-dev avrdude gcc-avr binutils-avr avr-libc \
+python3 py3-virtualenv \
+python3-dev freetype-dev fribidi-dev harfbuzz-dev jpeg-dev lcms2-dev openjpeg-dev tcl-dev tiff-dev tk-dev zlib-dev \
+jq patch libsodium caddy caddy-openrc curl polkit grep iproute2
 
 case $CLIENT in
   fluidd)
